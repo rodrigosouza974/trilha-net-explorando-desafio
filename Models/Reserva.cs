4446bf111691f522020ec36c8156a321b7c703f1
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-08-27 20:41:02 -0300

    desafio concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,8 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                Console.WriteLine($"Quantidade de Hospedes é superior a capacidade da Suite de {Suite.Capacidade} pessoas");
+
             }
         }
 
@@ -37,7 +39,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -46,15 +48,17 @@ namespace DesafioProjetoHospedagem.Models
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
-
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
 
-            return valor;
+            if (DiasReservados >= 10)
+            {
+                return valor = DiasReservados * Suite.ValorDiaria * 0.9m;
+            }
+            else
+            {
+               return valor = DiasReservados * Suite.ValorDiaria;
+            }
         }
     }
 }
